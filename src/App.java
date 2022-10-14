8f9af01a652cc76080a66737720bf860b1744b36
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-10-14 13:59:02 -0300

    refactor: returns an ordered list instead of a set in atoms()

--- src/App.java
+++ src/App.java
@@ -9,12 +9,13 @@ public class App {
     public static void main(String[] args) throws Exception {
         Atomic p = new Atomic("p");
         Atomic q = new Atomic("q");
+        Atomic r = new Atomic("r");
         
         Or o = new Or(p, q);
-        And a = new And(p, q);
+        And a = new And(p, r);
         Implies i = new Implies(o, a);
         Not n = new Not(i);
-        
+        System.out.println(n);
         System.out.println(Functions.atoms(n));
 
     }
--- src/functions/Functions.java
+++ src/functions/Functions.java
@@ -1,19 +1,25 @@
 package functions;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import abstractions.formulas.*;
-import abstractions.operators.*;
+import abstractions.formulas.Atomic;
+import abstractions.formulas.Formula;
+import abstractions.operators.And;
+import abstractions.operators.Implies;
+import abstractions.operators.Not;
+import abstractions.operators.Or;
 
 public class Functions {
 
-    private static Set<String> listAtoms = new HashSet<String>();
+    private static Set<String> setAtoms = new HashSet<String>();
     
     //returns a set of atoms from a formula
-    public static Set<String> atoms(Formula formula) {
+    public static List<String> atoms(Formula formula) {
         if (formula instanceof Atomic) {
-            listAtoms.add(formula.toString());
+            setAtoms.add(formula.toString());
         }
         if (formula instanceof Not) {
             Not negation = (Not) formula;
@@ -34,6 +40,10 @@ public class Functions {
             atoms(implies.getLeft());
             atoms(implies.getRight());
         }
+
+        List<String> listAtoms = setAtoms.stream()
+                                         .collect(Collectors.toList());
+
         return listAtoms;
     }
 
