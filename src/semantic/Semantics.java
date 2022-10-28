f21c4693b82bb7e5a7d6382dbb4e1fca64e84a9c
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-10-28 00:49:43 -0300

    feat: add bigAnd operator

--- src/semantic/Semantics.java
+++ src/semantic/Semantics.java
@@ -1,9 +1,21 @@
 package semantic;
 
+import java.util.List;
+
+import abstractions.formulas.Formula;
+import abstractions.operators.And;
+
 public class Semantics {
     
-    public static void atoms(){
-        
+    public static And bigAnd(List<Formula> formulasList){
+        Formula conjunction = formulasList.get(0);
+
+        formulasList.remove(0);
+        for(Formula formula : formulasList){
+            conjunction = new And(conjunction, formula);
+        }
+
+        return (And) conjunction;
     }
 
 }
