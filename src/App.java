b9963adbec307eaf1860181f9058464510d491d1
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-10-28 01:00:48 -0300

    feat: add bigOr operator

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import abstractions.formulas.Atomic;
+import abstractions.formulas.Formula;
 import abstractions.operators.And;
 import abstractions.operators.Implies;
 import abstractions.operators.Not;
 import abstractions.operators.Or;
 import functions.Functions;
+import semantic.Semantics;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -20,7 +24,7 @@ public class App {
         And a = new And(o, i);
         And b = new And(a, n);
 
-        System.out.println(b.toString());
+        //System.out.println(b.toString());
         //System.out.println(Functions.atoms(n));
 
         
@@ -34,6 +38,14 @@ public class App {
 
         //System.out.println(Functions.truthValue(n, interpretation));
         
-        System.out.println(Functions.satisfabilityBruteForce(b));
+        //System.out.println(Functions.satisfabilityBruteForce(b));
+
+        List<Formula> list = new ArrayList<>();
+        list.add(p);
+        list.add(q);
+        list.add(r);
+        list.add(s);
+
+        System.out.println(Semantics.bigAnd(list));
     }
 }
--- src/semantic/Semantics.java
+++ src/semantic/Semantics.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import abstractions.formulas.Formula;
 import abstractions.operators.And;
+import abstractions.operators.Or;
 
 public class Semantics {
     
@@ -16,6 +17,17 @@ public class Semantics {
         }
 
         return (And) conjunction;
+    }
+
+    public static Or bigOr(List<Formula> formulasList){
+        Formula disjunction = formulasList.get(0);
+
+        formulasList.remove(0);
+        for(Formula formula : formulasList){
+            disjunction = new Or(disjunction, formula);
+        }
+
+        return (Or) disjunction;
     }
 
 }
